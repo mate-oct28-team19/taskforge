72a8befb2e478c27f54f6fc23c41089a92602338
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-10-29 13:24:28 +0200

    liquibase added

--- src/main/java/com/example/taskforge/TaskforgeApplication.java
+++ src/main/java/com/example/taskforge/TaskforgeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskforgeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TaskforgeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TaskforgeApplication.class, args);
+    }
 }
