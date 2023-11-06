87e45dfad73af72f9b2e455126f1487029ee979c
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-11-06 19:40:34 +0200

    open api added

--- src/main/java/com/example/taskforge/controller/AuthController.java
+++ src/main/java/com/example/taskforge/controller/AuthController.java
@@ -6,6 +6,8 @@ import com.example.taskforge.dto.user.UserRegistrationRequestDto;
 import com.example.taskforge.dto.user.UserRegistrationResponseDto;
 import com.example.taskforge.exception.RegistrationException;
 import com.example.taskforge.security.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,18 +19,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication management", description = "Endpoints to register and authentication")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "Login", description = "Login into the system")
     @PostMapping("/login")
     public UserLoginResponseDto login(@RequestBody UserLoginRequestDto requestDto)
             throws RegistrationException {
         return authenticationService.authenticate(requestDto);
     }
 
+    @Operation(summary = "Register", description = "Register in the system")
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public UserRegistrationResponseDto register(
@@ -37,21 +42,25 @@ public class AuthController {
         return authenticationService.register(registrationRequest);
     }
 
+    @Operation(summary = "Confirm registration",
+            description = "Confirm registration via link from email")
     @GetMapping("/confirm")
     @ResponseStatus(HttpStatus.OK)
     public String confirm(@RequestParam("token") String token) throws RegistrationException {
         return authenticationService.confirmRegistrationToken(token);
     }
 
+    @Operation(summary = "Regenerate & resend code",
+            description = "Regenerates and resends confirmation code on email")
     @GetMapping("/resend")
     public UserRegistrationResponseDto resendCode(@RequestParam("token") String token)
             throws RegistrationException {
         return authenticationService.resend(token);
     }
 
-    @PostMapping("/confirm")
-    @ResponseStatus(HttpStatus.OK)
-    public void confirmCode(@RequestParam String token) throws RegistrationException {
-        authenticationService.enableUser(token);
-    }
+    //  @PostMapping("/confirm")
+    //  @ResponseStatus(HttpStatus.OK)
+    //  public void confirmCode(@RequestParam String token) throws RegistrationException {
+    //      authenticationService.enableUser(token);
+    //  }
 }
