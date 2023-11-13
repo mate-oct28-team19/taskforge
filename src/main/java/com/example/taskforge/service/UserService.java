f5aa3337603a451a0fd8b9be43b40853c057ed93
silchenko-arsen <silchenkoarsen@gmail.com>
2023-11-13 16:11:14 +0200

    fix checkstyle

--- src/main/java/com/example/taskforge/service/UserService.java
+++ src/main/java/com/example/taskforge/service/UserService.java
@@ -1,9 +1,8 @@
 package com.example.taskforge.service;
 
 import com.example.taskforge.model.User;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserService {
 
