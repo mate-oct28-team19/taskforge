028972d7f20653993858682c4897a18b1ee238bd
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-12-11 18:56:45 +0200

    email properties comes from application.properties file

--- src/main/java/com/example/taskforge/config/MailConfig.java
+++ src/main/java/com/example/taskforge/config/MailConfig.java
@@ -1,24 +1,34 @@
 package com.example.taskforge.config;
 
 import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@Component
+@PropertySource("classpath:application.properties")
 public class MailConfig {
+
+    @Value("${spring.mail.port}")
+    private int port;
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        String host = "smtp.gmail.com";
         mailSender.setHost(host);
-        int port = 587;
         mailSender.setPort(port);
-        String username = "taskforge19@gmail.com";
         mailSender.setUsername(username);
-        String password = "dwop euow jdbv ydwz";
         mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
