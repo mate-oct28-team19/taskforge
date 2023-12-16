9effbfa0ae59263c2a1f07c9592427f769f81f99
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-12-16 16:37:20 +0200

    getProperties endpoint added

--- src/main/java/com/example/taskforge/controller/UserController.java
+++ src/main/java/com/example/taskforge/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.taskforge.controller;
 import com.example.taskforge.dto.UpdateColorSchemeRequestDto;
 import com.example.taskforge.dto.UpdateLanguageRequestDto;
 import com.example.taskforge.dto.UpdatePasswordRequestDto;
+import com.example.taskforge.dto.user.UserPropertiesResponseDto;
 import com.example.taskforge.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,14 +36,14 @@ public class UserController {
     @Operation(summary = "Update color scheme", description = "Updates color scheme in db for certain user")
     @PatchMapping("/color_scheme")
     @ResponseStatus(HttpStatus.OK)
-        void changeColor(@RequestBody @Valid UpdateColorSchemeRequestDto dto, Authentication authentication) {
+    void changeColor(@RequestBody @Valid UpdateColorSchemeRequestDto dto, Authentication authentication) {
         userService.updateColor(authentication, dto);
     }
 
     @Operation(summary = "Update language", description = "Updates language in db for certain user")
     @PatchMapping("/language")
     @ResponseStatus(HttpStatus.OK)
-        void changeLanguage(@RequestBody @Valid UpdateLanguageRequestDto dto, Authentication authentication) {
+    void changeLanguage(@RequestBody @Valid UpdateLanguageRequestDto dto, Authentication authentication) {
         userService.updateLanguage(authentication, dto);
     }
 
@@ -51,4 +53,12 @@ public class UserController {
     void changePassword(@RequestBody @Valid UpdatePasswordRequestDto dto, Authentication authentication) {
         userService.updatePassword(authentication, dto);
     }
+
+    @Operation(summary = "Get color theme and language", description = "Returnes color theme and language for certain user")
+    @GetMapping("/properties")
+    @ResponseStatus(HttpStatus.OK)
+    UserPropertiesResponseDto getProperties(@RequestBody Authentication authentication) {
+        return userService.getProperties(authentication);
+    }
+
 }
